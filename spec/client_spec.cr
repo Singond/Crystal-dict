befa1c195c2a2fbd71367b00293f445eff6ae5a9
Jan Singon Slany <singond@seznam.cz>
2023-02-26 20:18:12 +0100

    Add client test

---
+++ spec/client_spec.cr
@@ -0,0 +1,54 @@
+require "spec"
+require "http"
+
+require "./spec_helper"
+require "./test_server"
+
+module DICT
+  # Modified Client which enables simulating delay between
+  # writing a request and reading a response.
+  class SlowClient < Client
+    def define(word : String, database : String)
+      puts "Sending request"
+      @input.send(Request.new(word, database))
+      sleep 2 if word == "slow"
+      resp = @output.receive
+      puts "Got response"
+      resp
+    end
+  end
+end
+
+describe DICT::Client do
+  describe "#define" do
+    it "retrieves the definition of word" do
+      server = TestServer.new
+      client = DICT::Client.new(server.io)
+      resp = client.define("lattice", "!")
+      puts resp
+    end
+
+    pending "matches correct response to each request" do
+      server = TestServer.new
+      client = DICT::SlowClient.new(server.io)
+      c = Channel(Tuple(String, DICT::Response)).new
+      spawn do
+        response = client.define("slow", "!")
+        c.send({"slow", response})
+      end
+      spawn do
+        response = client.define("lattice", "!")
+        c.send({"lattice", response})
+      end
+      2.times do
+        word, response = c.receive
+        case word
+        when "slow"
+          response.to_s.should match /not fast/
+        when "lattice"
+          response.to_s.should match /arrangement of atoms/
+        end
+      end
+    end
+  end
+end
---
+++ spec/test_server.cr
@@ -0,0 +1,62 @@
+class TestServer
+  @@words = Hash(String, String).new
+
+  def initialize
+    @in, @inw = IO.pipe
+    @outr, @out = IO.pipe
+    spawn do
+      run
+    end
+  end
+
+  def io
+    IO::Stapled.new(@outr, @inw)
+  end
+
+  def run
+    @out << "220 localhost testing server <auth.mime> <ok@localhost>\n\n"
+    while req = @in.gets
+      if req =~ /define ! ([a-z]+)/
+        word = $~[1]
+        if @@words.has_key? word
+          @out << @@words[word] << "\n.\n"
+          puts "Sent response of #{@@words[word].size} characters"
+        else
+          @out << "552 No match" << "\n.\n"
+        end
+      end
+    end
+  end
+
+  @@words["lattice"] = <<-END
+  150 1 definitions retrieved
+  151 "Lattice" gcide "The Collaborative International Dictionary of English v.0.48"
+  Lattice \\Lat"tice\\, n. [OE. latis, F. lattis lathwork, fr. latte
+    lath. See {Latten}, 1st {Lath}.]
+    1. (Crystallography) The arrangement of atoms or molecules in
+      a crystal, represented as a repeating arrangement of
+      points in space, each point representing the location of
+      an atom or molecule; called also {crystal lattice} and
+      {space lattice}.
+      [PJC]
+  .
+  END
+
+  @@words["monoclinic"] = <<-END
+  150 1 definitions retrieved
+  151 "monoclinic" wn "WordNet (r) 3.0 (2006)"
+  monoclinic
+      adj 1: having three unequal crystal axes with one oblique
+            intersection; "monoclinic system" [ant: {anorthic},
+            {triclinic}]
+  .
+  END
+
+  @@words["slow"] = <<-END
+  150 1 definitions retrieved
+  151 "slow" md "My Dictionary 0.4"
+  slow
+      adj 1. not fast
+  .
+  END
+end
