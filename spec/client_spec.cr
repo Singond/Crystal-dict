5a9934be26af0b1a7a56ff3e58de481107f1a3e1
Jan Singon Slany <singond@seznam.cz>
2023-08-04 22:54:32 +0200

    Test response object attributes

--- spec/client_spec.cr
+++ spec/client_spec.cr
@@ -24,15 +24,11 @@ describe DICT::Client do
       server = TestServer.new
       client = DICT::Client.new(server.io)
       resp = client.define("lattice", "!")
+
       resp.should be_a DICT::DefinitionsResponse
       resp = resp.as DICT::DefinitionsResponse
-      resp.definitions.size.should eq 1
+
       d = resp.definitions[0]
-      d.word.should eq "Lattice"
-      d.dbname.should eq "gcide"
-      d.dbdesc.should eq \
-          "The Collaborative International Dictionary of English v.0.48"
-      d.body.should start_with "Lattice"
       d.body.should match /The arrangement of atoms or molecules/
     end
   end
@@ -58,5 +54,37 @@ describe DICT::Client do
         response.to_s.should match /arrangement of atoms/
       end
     end
+  end
+end
+
+describe DICT::DefinitionsResponse do
+  it "parses a 150 response correctly" do
+    resp = DICT::DefinitionsResponse.new(150, IO::Memory.new(<<-END))
+    1 definitions retrieved
+    151 "Lattice" gcide "The Collaborative International Dictionary of English v.0.48"
+    Lattice \\Lat"tice\\, n. [OE. latis, F. lattis lathwork, fr. latte
+      lath. See {Latten}, 1st {Lath}.]
+      1. (Crystallography) The arrangement of atoms or molecules in
+        a crystal, represented as a repeating arrangement of
+        points in space, each point representing the location of
+        an atom or molecule; called also {crystal lattice} and
+        {space lattice}.
+        [PJC]
+    .
+
+    END
+
+    resp.status.should eq DICT::Status::DEFINITIONS_LIST
+    resp.status_message.should eq "1 definitions retrieved"
+    resp = resp.as DICT::DefinitionsResponse
+    resp.definitions.size.should eq 1
+
+    d = resp.definitions[0]
+    d.status.should eq DICT::Status::DEFINITION
+    d.word.should eq "Lattice"
+    d.dbname.should eq "gcide"
+    d.dbdesc.should eq \
+        "The Collaborative International Dictionary of English v.0.48"
+    d.body.should start_with "Lattice"
   end
 end
